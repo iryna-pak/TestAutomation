2b3fa548c4980246692b2f7ac9c6c64ba8403f98
iryna-pak <iryna.pak@gmail.com>
2022-01-18 17:07:51 +0100

    Homework2 is done

---
+++ src/test/java/Lesson2/AppTest.java
@@ -0,0 +1,13 @@
+package Lesson2;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("AppTest");
+        assertTrue( true );
+    }
+
+}
