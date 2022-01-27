122e6b15bc37c920d61e70f87d3b977839862a41
iryna-pak <iryna.pak@gmail.com>
2022-01-27 13:35:19 +0100

    Lesson4 online

---
+++ src/main/java/Lesson4/Box.java
@@ -0,0 +1,31 @@
+package Lesson4;
+
+public class Box {
+    private Integer ballsCounter;
+
+    public Box() {
+        ballsCounter = 0;
+    }
+
+    public Integer getBallsCounter() {
+        return ballsCounter;
+    }
+
+    public void addBall() {
+        ballsCounter ++;
+    }
+
+    public void removeBall() {
+        if (ballsCounter == 0) {
+            throw new NullPointerException("Уже 0 мячей!!!");
+        }
+        ballsCounter --;
+    }
+
+    public void shuffleBalls() throws EmptyBoxException {
+        if (ballsCounter == 0) {
+            throw new EmptyBoxException();
+        }
+    }
+
+}
